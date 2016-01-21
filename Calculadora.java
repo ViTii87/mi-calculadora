b1e2e4c93400ec69ca1fbb2b2cf6e815cfa0ab0f
Victor <victormel87@gmail.com>
2016-01-21 10:40:26 +0100

    Commit inicial del proyecto

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Calculadora here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calculadora
+{
+
+    /**
+     * Constructor de objetos calculadora
+     */
+    public Calculadora()
+    {
+    
+    }
+
+}
